122292705fbd7c08644ef2e6ec816c87c6ac3016
Harut <har@Haruts-MacBook-Pro.local>
2014-12-29 02:44:41 +0400

    App with getting address from current location and SMS sending functionality

---
+++ CoordViaSMS/CoordViaSMSTests/CoordViaSMSTests.swift
@@ -0,0 +1,36 @@
+//
+//  CoordViaSMSTests.swift
+//  CoordViaSMSTests
+//
+//  Created by Harut on 29.12.14.
+//  Copyright (c) 2014 Harut Arakelyan. All rights reserved.
+//
+
+import UIKit
+import XCTest
+
+class CoordViaSMSTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        XCTAssert(true, "Pass")
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock() {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
